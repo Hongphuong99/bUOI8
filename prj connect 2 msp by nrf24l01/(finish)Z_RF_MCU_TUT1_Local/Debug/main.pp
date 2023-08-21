2270d6041983cdc774bc7ebe2d7e67fb436836f3
Hongphuong99 <49827019+Hongphuong99@users.noreply.github.com>
2023-08-21 13:12:21 +0700

    Add files via upload

---
+++ prj connect 2 msp by nrf24l01/(finish)Z_RF_MCU_TUT1_Local/Debug/main.pp
@@ -0,0 +1,25 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/ccs_base/msp430/include/msp430G2553.h
+main.obj: F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/ccs_base/msp430/include/in430.h
+main.obj: F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/tools/compiler/msp430_4.1.2/include/stdio.h
+main.obj: F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/tools/compiler/msp430_4.1.2/include/linkage.h
+main.obj: F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/tools/compiler/msp430_4.1.2/include/stdarg.h
+main.obj: ../UART.h
+main.obj: ../SPI.h
+main.obj: ../nrf24l01.h
+main.obj: F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/tools/compiler/msp430_4.1.2/include/stddef.h
+main.obj: ../SPI.h
+
+../main.c: 
+F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/ccs_base/msp430/include/msp430G2553.h: 
+F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/ccs_base/msp430/include/in430.h: 
+F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/tools/compiler/msp430_4.1.2/include/stdio.h: 
+F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/tools/compiler/msp430_4.1.2/include/linkage.h: 
+F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/tools/compiler/msp430_4.1.2/include/stdarg.h: 
+../UART.h: 
+../SPI.h: 
+../nrf24l01.h: 
+F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/tools/compiler/msp430_4.1.2/include/stddef.h: 
+../SPI.h: 
---
+++ prj connect 2 msp by nrf24l01/(finish)Z_RF_MCU_TUT1_Remote/Debug/SPI.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+SPI.obj: ../SPI.c
+SPI.obj: ../SPI.h
+SPI.obj: F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/ccs_base/msp430/include/msp430g2553.h
+SPI.obj: F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/ccs_base/msp430/include/in430.h
+
+../SPI.c: 
+../SPI.h: 
+F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
+F:/Softs/Texas\ Instrument/CCS\ 5.3/ccsv5/ccs_base/msp430/include/in430.h: 
